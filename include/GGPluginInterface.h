18256a4d17061d6645e0f3e8dfd463ab33aedfe9
T. Zachary Laine <whatwasthataddress@gmail.com>
2004-07-17 05:50:38 +0000

    Initial add.

---
+++ include/GGPluginInterface.h
@@ -0,0 +1,157 @@
+// -*- C++ -*-
+#ifndef _GGPluginInterface_h_
+#define _GGPluginInterface_h_
+
+#ifndef _GGBase_h_
+#include  "GGBase.h"
+#endif
+
+#ifndef LTDL_H
+#include "../../libltdl/ltdl.h"
+#endif
+
+#include <GGButton.h>
+#include <GGScroll.h>
+#include <GGSlider.h>
+#include <GGSpin.h>
+
+#include <string>
+
+namespace GG {
+
+class DropDownList;
+class DynamicGraphic;
+class Edit;
+class ListBox;
+class MenuBar;
+class MultiEdit;
+class StaticGraphic;
+class TextControl;
+
+/** the interface to custom-control plugins.  This class is used to access derived GG controls that are unknown until runtime, but are available for 
+    dynamic loading in shared libraries/DLLs.  The interface basically allows you to create custom controls (GG::Button subclasses, GG::ListBox 
+    subclasses, etc.) from one or more dynamic libraries, which in turn allows you to change the styles of the controls in an application without 
+    recompiling, or even relinking.  While the interface is in an unloaded 
+    state, the functions in the interface are all null, and calling any of them will crash your app.  Once a plugin has been loaded, all the 
+    functions in the interface should be valid (if the plugin author did her job).  Loading a plugin also adds all the XML generator functions provided
+    by the plugin to the singleton GG::App's XML object factory.  Unloading a plugin removes all such generators from the factory. */
+class GG_API PluginInterface
+{
+public:
+    typedef const char*             (*PluginNameFn)();
+
+    typedef const char*             (*DefaultFontNameFn)();
+    typedef int                     (*DefaultFontSizeFn)();
+
+    typedef GG::Button*             (*CreateButtonFn)(int, int, int, int, const std::string&, const std::string&, int, GG::Clr, GG::Clr, Uint32);
+    typedef GG::StateButton*        (*CreateStateButtonFn)(int, int, int, int, const std::string&, const std::string&, int, Uint32, 
+                                                           GG::Clr, GG::Clr, GG::Clr, GG::StateButton::StateButtonStyle, int, int, int, int, Uint32);
+    typedef GG::RadioButtonGroup*   (*CreateRadioButtonGroupFn)(int, int);
+    typedef GG::DropDownList*       (*CreateDropDownListFn)(int, int, int, int, int, GG::Clr, GG::Clr, Uint32);
+    typedef GG::DynamicGraphic*     (*CreateDynamicGraphicFn)(int, int, int, int, bool, int, int, int, const boost::shared_ptr<GG::Texture>&, Uint32, int, Uint32);
+    typedef GG::Edit*               (*CreateEditFn)(int, int, int, int, const std::string&, const std::string&, int, GG::Clr, GG::Clr, GG::Clr, Uint32);
+    typedef GG::ListBox*            (*CreateListBoxFn)(int, int, int, int, GG::Clr, GG::Clr, Uint32);
+    typedef GG::MenuBar*            (*CreateMenuBarFn)(int, int, int, const std::string&, int, GG::Clr, GG::Clr, GG::Clr);
+    typedef GG::MultiEdit*          (*CreateMultiEditFn)(int, int, int, int, const std::string&, const std::string&, int, GG::Clr, Uint32, GG::Clr, GG::Clr, Uint32);
+    typedef GG::Scroll*             (*CreateScrollFn)(int, int, int, int, GG::Scroll::Orientation, GG::Clr, GG::Clr, Uint32);
+    typedef GG::Slider*             (*CreateSliderFn)(int, int, int, int, int, int, GG::Slider::Orientation, GG::Slider::LineStyleType, GG::Clr, int, int, Uint32);
+    typedef GG::Spin<int>*          (*CreateIntSpinFn)(int, int, int, int, int, int, int, int, bool, const std::string&, int, GG::Clr, GG::Clr, GG::Clr, Uint32);
+    typedef GG::Spin<double>*       (*CreateDoubleSpinFn)(int, int, int, int, double, double, double, double, bool, const std::string&, int, GG::Clr, GG::Clr, GG::Clr, Uint32);
+    typedef GG::StaticGraphic*      (*CreateStaticGraphicFn)(int, int, int, int, const boost::shared_ptr<GG::Texture>&, Uint32, Uint32);
+    typedef GG::TextControl*        (*CreateTextControlFn)(int, int, int, int, const std::string&, const std::string&, int pts, Uint32, GG::Clr, Uint32);
+
+    typedef void                    (*DestroyControlFn)(GG::Wnd*);
+
+    typedef const std::map<std::string, std::string>& 
+                                    (*FactoryGeneratorNamesFn)();
+
+    /** \name Structors */ //@{
+    PluginInterface(); ///< default ctor.
+    
+    /** ctor that loads the plugin file \a lib_name.  The base filename should be provided, without the extension (i.e. "foo", 
+        not "foo.so" or "foo.dll"). */
+    PluginInterface(const std::string& lib_name);
+    
+    ~PluginInterface(); ///< dtor.
+    //@}
+
+    /** \name Accessors */ //@{
+    /** returns true iff this PluginInterface has a loaded plugin.  This is a conversion operator, allowing you to test the validity of 
+        the interface, as you would a pointer (e.g. if (my_interface) my_interface->PluginName();).  \warning If this method returns false, 
+        the functions in the interface are invalid. */
+    operator bool() const;
+    //@}
+
+    /** \name Mutators */ //@{
+    /** loads the plugin \a lib_name, unloading the currently-loaded plugin if necessary. */
+    bool Load(const std::string& lib_name);
+
+    PluginNameFn                PluginName;             ///< returns the name of this plugin
+
+    DefaultFontNameFn           DefaultFontName;        ///< returns the default font name that should be used to create controls using this plugin.
+    DefaultFontSizeFn           DefaultFontSize;        ///< returns the default font point size that should be used to create controls using this plugin.
+
+    CreateButtonFn              CreateButton;           ///< returns a new GG::Button, or an instance of a plugin-specific subclass of GG::Button.
+    CreateStateButtonFn         CreateStateButton;      ///< returns a new GG::StateButton, or an instance of a plugin-specific subclass of GG::StateButton.
+    CreateRadioButtonGroupFn    CreateRadioButtonGroup; ///< returns a new GG::DropDownList, or an instance of a plugin-specific subclass of GG::DropDownList.
+    CreateDropDownListFn        CreateDropDownList;     ///< returns a new GG::DropDownList, or an instance of a plugin-specific subclass of GG::DropDownList.
+    CreateDynamicGraphicFn      CreateDynamicGraphic;   ///< returns a new GG::DynamicGraphic, or an instance of a plugin-specific subclass of GG::DynamicGraphic.
+    CreateEditFn                CreateEdit;             ///< returns a new GG::Edit, or an instance of a plugin-specific subclass of GG::Edit.
+    CreateListBoxFn             CreateListBox;          ///< returns a new GG::ListBox, or an instance of a plugin-specific subclass of GG::ListBox.
+    CreateMenuBarFn             CreateMenuBar;          ///< returns a new GG::MenuBar, or an instance of a plugin-specific subclass of GG::MenuBar.
+    CreateMultiEditFn           CreateMultiEdit;        ///< returns a new GG::MultiEdit, or an instance of a plugin-specific subclass of GG::MultiEdit.
+    CreateScrollFn              CreateScroll;           ///< returns a new GG::Scroll, or an instance of a plugin-specific subclass of GG::Scroll.
+    CreateSliderFn              CreateSlider;           ///< returns a new GG::Slider, or an instance of a plugin-specific subclass of GG::Slider.
+    CreateIntSpinFn             CreateIntSpin;          ///< returns a new GG::Spin<int>, or an instance of a plugin-specific subclass of GG::Spin<int>.
+    CreateDoubleSpinFn          CreateDoubleSpin;       ///< returns a new GG::Spin<double>, or an instance of a plugin-specific subclass of GG::Spin<double>.
+    CreateStaticGraphicFn       CreateStaticGraphic;    ///< returns a new GG::StaticGraphic, or an instance of a plugin-specific subclass of GG::StaticGraphic.
+    CreateTextControlFn         CreateTextControl;      ///< returns a new GG::TextControl, or an instance of a plugin-specific subclass of GG::TextControl.
+
+    DestroyControlFn            DestroyControl;         ///< destroys a new GG::Control-derived created by the plugin.
+
+    /** returns a map from XML tag names of plugin-specific Control classes to the names of the generator functions for those classes. */
+    FactoryGeneratorNamesFn     FactoryGeneratorNames;
+    //@}
+
+private:
+    lt_dlhandle m_handle;
+};
+
+/** This singleton class is essentially a very thin wrapper around a map of PluginInterface smart pointers, keyed on std::string plugin names.  
+    The user need only request a plugin through GetPlugin(); if the plugin is not already resident, it will be loaded.*/
+class GG_API PluginManager
+{
+public:
+    /** \name Structors */ //@{
+    PluginManager(); ///< ctor
+    //@}
+
+    /** \name Mutators */ //@{
+    shared_ptr<PluginInterface> GetPlugin(const string& name);  ///< returns a shared_ptr to the plugin interface created from plugin \a name. If the plugin is not present in the manager's pool, it will be loaded from disk.
+
+    /** removes the manager's shared_ptr to the plugin created from file \a name, if it exists.  \note Due to shared_ptr semantics, 
+        the plugin may not be deleted until much later. */
+    void                FreePlugin(const string& name);
+    //@}
+
+    /** initializes the dynamic loader system that loads and unloads plugins.  This is available as a convenience only; it will be called 
+        automatically as needed. */
+    static void InitDynamicLoader();
+
+    /** adds a directory which should be searched for plugins. */
+    static void AddSearchDirectory(const std::string& dir);
+
+    /** cleans up the dynamic loader system that loads and unloads plugins.  This should be called manually when desiredl it will never be called 
+        by other PluginInterface code. */
+    static void CleanupDynamicLoader();
+
+private:
+    static bool s_created;
+    static bool s_lt_dl_initialized;
+    map<string, shared_ptr<PluginInterface> >
+                m_plugins;
+};
+
+} // namespace GG
+
+#endif // _GGPluginInterface_h_
---
+++ src/GGPluginInterface.cpp
@@ -0,0 +1,207 @@
+#include "GGPluginInterface.h"
+
+#include <GGDropDownList.h>
+#include <GGDynamicGraphic.h>
+#include <GGEdit.h>
+#include <GGListBox.h>
+#include <GGMenu.h>
+#include <GGMultiEdit.h>
+#include <GGStaticGraphic.h>
+#include <GGTextControl.h>
+
+using namespace GG;
+
+///////////////////////////////////////
+// class GG::PluginInterface
+///////////////////////////////////////
+PluginInterface::PluginInterface() : 
+    m_handle(0) 
+{
+}
+
+PluginInterface::PluginInterface(const std::string& lib_name) : 
+    m_handle(0) 
+{
+    Load(lib_name);
+}
+
+PluginInterface::~PluginInterface()
+{
+    if (m_handle) {
+        GG::App* app = GG::App::GetApp();
+        const std::map<std::string, std::string>& generator_names = FactoryGeneratorNames();
+        for (std::map<std::string, std::string>::const_iterator it = generator_names.begin(); it != generator_names.end(); ++it) {
+            app->RemoveWndGenerator(it->first);
+        }
+
+        // plugin name
+        PluginName = 0;
+
+        // plugin defaults
+        DefaultFontName = 0;
+        DefaultFontSize = 0;
+
+        // direct object creation functions, and direct object destruction function
+        CreateButton = 0;
+        CreateStateButton = 0;
+        CreateRadioButtonGroup = 0;
+        CreateDropDownList = 0;
+        CreateDynamicGraphic = 0;
+        CreateEdit = 0;
+        CreateListBox = 0;
+        CreateMenuBar = 0;
+        CreateMultiEdit = 0;
+        CreateScroll = 0;
+        CreateSlider = 0;
+        CreateIntSpin = 0;
+        CreateDoubleSpin = 0;
+        CreateStaticGraphic = 0;
+        CreateTextControl = 0;
+        DestroyControl = 0;
+
+        FactoryGeneratorNames = 0;
+
+        lt_dlclose(m_handle);
+        m_handle = 0;
+    }
+}
+
+PluginInterface::operator bool() const
+{
+    return m_handle;
+}
+
+bool PluginInterface::Load(const std::string& lib_name)
+{
+    PluginManager::InitDynamicLoader();
+
+    bool retval = true;
+
+    int err = 0;
+    if (m_handle) {
+        if (err = lt_dlclose(m_handle)) {
+            retval = false;
+            GG::App::GetApp()->Logger().errorStream() 
+                << "PluginInterface::Load : lt_dlclose() call failed; load of new dynamic library aborted (error #" 
+                << err << ": " << lt_dlerror() << ").";
+        }
+    }
+
+    if (!err) {
+        m_handle = lt_dlopenext(lib_name.c_str());
+        if (m_handle) {
+            // plugin name
+            PluginName = (PluginNameFn)(lt_dlsym(m_handle, "PluginName"));
+
+            // plugin defaults
+            DefaultFontName = (DefaultFontNameFn)(lt_dlsym(m_handle, "DefaultFontName"));
+            DefaultFontSize = (DefaultFontSizeFn)(lt_dlsym(m_handle, "DefaultFontSize"));
+
+            // direct object creation functions, and direct object destruction function
+            CreateButton = (CreateButtonFn)(lt_dlsym(m_handle, "CreateButton"));
+            CreateStateButton = (CreateStateButtonFn)(lt_dlsym(m_handle, "CreateStateButton"));
+            CreateRadioButtonGroup = (CreateRadioButtonGroupFn)(lt_dlsym(m_handle, "CreateRadioButtonGroup"));
+            CreateDropDownList = (CreateDropDownListFn)(lt_dlsym(m_handle, "CreateDropDownList"));
+            CreateDynamicGraphic = (CreateDynamicGraphicFn)(lt_dlsym(m_handle, "CreateDynamicGraphic"));
+            CreateEdit = (CreateEditFn)(lt_dlsym(m_handle, "CreateEdit"));
+            CreateListBox = (CreateListBoxFn)(lt_dlsym(m_handle, "CreateListBox"));
+            CreateMenuBar = (CreateMenuBarFn)(lt_dlsym(m_handle, "CreateMenuBar"));
+            CreateMultiEdit = (CreateMultiEditFn)(lt_dlsym(m_handle, "CreateMultiEdit"));
+            CreateScroll = (CreateScrollFn)(lt_dlsym(m_handle, "CreateScroll"));
+            CreateSlider = (CreateSliderFn)(lt_dlsym(m_handle, "CreateSlider"));
+            CreateIntSpin = (CreateIntSpinFn)(lt_dlsym(m_handle, "CreateIntSpin"));
+            CreateDoubleSpin = (CreateDoubleSpinFn)(lt_dlsym(m_handle, "CreateDoubleSpin"));
+            CreateStaticGraphic = (CreateStaticGraphicFn)(lt_dlsym(m_handle, "CreateStaticGraphic"));
+            CreateTextControl = (CreateTextControlFn)(lt_dlsym(m_handle, "CreateTextControl"));
+            DestroyControl = (DestroyControlFn)(lt_dlsym(m_handle, "DestroyControl"));
+
+            // get the names of the XML factory generator functions required to create GG::Wnd-derived classes included in the plugin from XML descriptions
+            FactoryGeneratorNames = (FactoryGeneratorNamesFn)(lt_dlsym(m_handle, "FactoryGeneratorNames"));
+            typedef GG::XMLObjectFactory<GG::Wnd>::Generator GeneratorFn;
+            GG::App* app = GG::App::GetApp();
+            const std::map<std::string, std::string>& generator_names = FactoryGeneratorNames();
+            for (std::map<std::string, std::string>::const_iterator it = generator_names.begin(); it != generator_names.end(); ++it) {
+                app->AddWndGenerator(it->first, (GeneratorFn)(lt_dlsym(m_handle, it->second.c_str())));
+            }
+        } else {
+            retval = false;
+            GG::App::GetApp()->Logger().errorStream() 
+                << "PluginInterface::Load : Failed to load dynamic library \"" << lib_name << "\" (error was: " << lt_dlerror() << ").";
+        }
+    }
+    return retval;
+}
+
+///////////////////////////////////////
+// class GG::PluginManager
+///////////////////////////////////////
+// static member(s)
+bool PluginManager::s_created = false;
+bool PluginManager::s_lt_dl_initialized = false;
+
+PluginManager::PluginManager()
+{
+    if (s_created)
+        throw std::runtime_error("Attempted to create a second instance of GG::PluginManager");
+    s_created = true;
+}
+
+shared_ptr<PluginInterface> PluginManager::GetPlugin(const string& name)
+{
+    std::map<string, shared_ptr<PluginInterface> >::iterator it = m_plugins.find(name);
+    if (it == m_plugins.end()) { // if no such plugin was found, attempt to load it now, using name as the filename
+        m_plugins[name].reset(new PluginInterface);
+        m_plugins[name]->Load(name);
+        return m_plugins[name];
+    } else { // otherwise, just return the plugin we found
+        return it->second;
+    }
+}
+
+void PluginManager::FreePlugin(const string& name)
+{
+    std::map<string, shared_ptr<PluginInterface> >::iterator it = m_plugins.find(name);
+    if (it != m_plugins.end())
+        m_plugins.erase(it);
+}
+
+void PluginManager::InitDynamicLoader()
+{
+    if (s_lt_dl_initialized)
+        return;
+
+    int err = lt_dlinit();
+    if (err) {
+        GG::App::GetApp()->Logger().errorStream() 
+            << "PluginManager::InitDynamicLoader : lt_dlinit() call failed. (error #" << err << ": " << lt_dlerror() << ").";
+    } else {
+        s_lt_dl_initialized = true;
+    }
+}
+
+void PluginManager::AddSearchDirectory(const std::string& dir)
+{
+    if (!s_lt_dl_initialized)
+        InitDynamicLoader();
+
+    int err = lt_dladdsearchdir(dir.c_str());
+    if (err) {
+        GG::App::GetApp()->Logger().errorStream() 
+            << "PluginManager::AddSearchDirectory : lt_dladdsearchdir() call failed for directory \"" << dir << "\". (error #" << err 
+            << ": " << lt_dlerror() << ").";
+    }
+}
+
+void PluginManager::CleanupDynamicLoader()
+{
+    if (!s_lt_dl_initialized)
+        return;
+
+    int err = lt_dlexit();
+    if (err) {
+        GG::App::GetApp()->Logger().errorStream() 
+            << "PluginManager::CleanupDynamicLoader : lt_dlexit() call failed. (error #" << err << ": " << lt_dlerror() << ").";
+    } else {
+        s_lt_dl_initialized = false;
+    }
+}

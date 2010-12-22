f76f02015c4f47bd0f2e0bb2493941d4bc281125
T. Zachary Laine <whatwasthataddress@gmail.com>
2010-12-22 04:41:51 +0000

    Fixed the excruciatingly badly written tests in TestStrongSizeTypedef.cpp.

--- test/TestStrongSizeTypedef.cpp
+++ test/TestStrongSizeTypedef.cpp
@@ -9,6 +9,8 @@ std::size_t i = 7;
 GG::StrSize x(2);
 
 std::size_t temp_size;
+std::size_t temp_size_1;
+std::size_t temp_size_2;
 
 #define ARITHMETIC_ITERATION(op)                                   \
     BOOST_CHECK_EQUAL(Value(i op x), i op Value(x));               \
@@ -20,8 +22,19 @@ std::size_t temp_size;
     x = GG::StrSize(2)
 
 #define ASSIGN_ARITHMETIC_ITERATION(op)                                 \
-    BOOST_CHECK_EQUAL(Value(x op i), (temp_size = Value(x)) op i); RESET(); \
-    BOOST_CHECK_EQUAL(Value(x op x), (temp_size = Value(x)) op (temp_size = Value(x))); RESET()
+    temp_size_1 = Value(x op i);                                        \
+    RESET();                                                            \
+    temp_size = Value(x);                                               \
+    temp_size_2 = temp_size op i;                                       \
+    RESET();                                                            \
+    BOOST_CHECK_EQUAL(temp_size_1, temp_size_2);                        \
+                                                                        \
+    temp_size_1 = Value(x op x);                                        \
+    RESET();                                                            \
+    temp_size = Value(x);                                               \
+    temp_size_2 = temp_size op temp_size;                               \
+    RESET();                                                            \
+    BOOST_CHECK_EQUAL(temp_size_1, temp_size_2)
 
 #define COMPARISON_ITERATION(op)                                 \
     BOOST_CHECK_EQUAL(i op x, i op Value(x));                    \
@@ -33,13 +46,18 @@ BOOST_AUTO_TEST_CASE( arithmetic )
     ARITHMETIC_ITERATION(+);
     ARITHMETIC_ITERATION(-);
     ARITHMETIC_ITERATION(*);
+
     BOOST_CHECK_EQUAL(Value(x / i), Value(x) / i);
     BOOST_CHECK_EQUAL(Value(x / x), Value(x) / Value(x));
+
+    BOOST_CHECK_EQUAL(Value(x % i), Value(x) % i);
+    BOOST_CHECK_EQUAL(Value(x % x), Value(x) % Value(x));
 
     ASSIGN_ARITHMETIC_ITERATION(+=);
     ASSIGN_ARITHMETIC_ITERATION(-=);
     ASSIGN_ARITHMETIC_ITERATION(*=);
     ASSIGN_ARITHMETIC_ITERATION(/=);
+    ASSIGN_ARITHMETIC_ITERATION(%=);
 }
 
 BOOST_AUTO_TEST_CASE( comparison )
@@ -54,13 +72,31 @@ BOOST_AUTO_TEST_CASE( comparison )
 
 BOOST_AUTO_TEST_CASE( unary )
 {
-    BOOST_CHECK_EQUAL(++x, ++(temp_size = Value(x)));
-    BOOST_CHECK_EQUAL(x++, (temp_size = Value(x))++);
-    BOOST_CHECK_EQUAL(x, temp_size);
+    GG::StrSize temp_x_1;
 
-    BOOST_CHECK_EQUAL(--x, --(temp_size = Value(x)));
-    BOOST_CHECK_EQUAL(x--, (temp_size = Value(x))--);
-    BOOST_CHECK_EQUAL(x, temp_size);
+    temp_x_1 = ++x;
+    RESET();
+    temp_size_2 = ++(temp_size = Value(x));
+    RESET();
+    BOOST_CHECK_EQUAL(temp_x_1, temp_size_2);
+
+    temp_x_1 = x++;
+    RESET();
+    temp_size_2 = (temp_size = Value(x))++;
+    RESET();
+    BOOST_CHECK_EQUAL(temp_x_1, temp_size_2);
+
+    temp_x_1 = --x;
+    RESET();
+    temp_size_2 = --(temp_size = Value(x));
+    RESET();
+    BOOST_CHECK_EQUAL(temp_x_1, temp_size_2);
+
+    temp_x_1 = x--;
+    RESET();
+    temp_size_2 = (temp_size = Value(x))--;
+    RESET();
+    BOOST_CHECK_EQUAL(temp_x_1, temp_size_2);
 
     BOOST_CHECK_EQUAL(!x, !Value(x));
 

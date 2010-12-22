132d9ee052a439d7a89832707cafa7dcada93fbe
T. Zachary Laine <whatwasthataddress@gmail.com>
2010-12-22 04:21:39 +0000

    Fixed the excruciatingly badly written tests in TestStrongIntegralTypedef.cpp.

--- test/TestStrongIntegralTypedef.cpp
+++ test/TestStrongIntegralTypedef.cpp
@@ -11,7 +11,11 @@ GG::X x(2);
 GG::X_d xd(5.1);
 
 int temp_int;
+int temp_int_1;
+int temp_int_2;
 double temp_double;
+double temp_double_1;
+double temp_double_2;
 
 #define ARITHMETIC_ITERATION(op)                                   \
     BOOST_CHECK_EQUAL(Value(i op x), i op Value(x));               \
@@ -35,15 +39,63 @@ double temp_double;
     xd = GG::X_d(5.1)
 
 #define ASSIGN_ARITHMETIC_ITERATION(op)                                 \
-    BOOST_CHECK_EQUAL(Value(x op i), (temp_int = Value(x)) op i); RESET(); \
-    BOOST_CHECK_EQUAL(Value(x op d), (temp_int = Value(x)) op d); RESET(); \
-    BOOST_CHECK_EQUAL(Value(x op x), (temp_int = Value(x)) op (temp_int = Value(x))); RESET(); \
-    BOOST_CHECK_EQUAL(Value(x op xd), (temp_int = Value(x)) op (temp_double = Value(xd))); RESET(); \
-    BOOST_CHECK_EQUAL(Value(xd op x), (temp_double = Value(xd)) op (temp_int = Value(x))); RESET(); \
+    temp_int_1 = Value(x op i);                                         \
+    RESET();                                                            \
+    temp_int = Value(x);                                                \
+    temp_int_2 = temp_int op i;                                         \
+    RESET();                                                            \
+    BOOST_CHECK_EQUAL(temp_int_1, temp_int_2);                          \
                                                                         \
-    BOOST_CHECK_EQUAL(Value(xd op i), (temp_double = Value(xd)) op i); RESET(); \
-    BOOST_CHECK_EQUAL(Value(xd op d), (temp_double = Value(xd)) op d); RESET(); \
-    BOOST_CHECK_EQUAL(Value(xd op xd), (temp_double = Value(xd)) op (temp_double = Value(xd))); RESET()
+    temp_double_1 = Value(x op d);                                      \
+    RESET();                                                            \
+    temp_int = Value(x);                                                \
+    temp_double_2 = temp_int op d;                                      \
+    RESET();                                                            \
+    BOOST_CHECK_EQUAL(temp_double_1, temp_double_2);                    \
+                                                                        \
+    temp_int_1 = Value(x op x);                                         \
+    RESET();                                                            \
+    temp_int = Value(x);                                                \
+    temp_int_2 = temp_int op temp_int;                                  \
+    RESET();                                                            \
+    BOOST_CHECK_EQUAL(temp_int_1, temp_int_2);                          \
+                                                                        \
+    temp_double_1 = Value(x op xd);                                     \
+    RESET();                                                            \
+    temp_int = Value(x);                                                \
+    temp_double = Value(xd);                                            \
+    temp_double_2 = temp_int op temp_double;                            \
+    RESET();                                                            \
+    BOOST_CHECK_EQUAL(temp_double_1, temp_double_2);                    \
+                                                                        \
+    temp_double_1 = Value(xd op x);                                     \
+    RESET();                                                            \
+    temp_double = Value(xd);                                            \
+    temp_int = Value(x);                                                \
+    temp_double_2 = temp_double op temp_int;                            \
+    RESET();                                                            \
+    BOOST_CHECK_EQUAL(temp_double_1, temp_double_2);                    \
+                                                                        \
+    temp_double_1 = Value(xd op i);                                     \
+    RESET();                                                            \
+    temp_double = Value(xd);                                            \
+    temp_double_2 = temp_double op i;                                   \
+    RESET();                                                            \
+    BOOST_CHECK_EQUAL(temp_double_1, temp_double_2);                    \
+                                                                        \
+    temp_double_1 = Value(xd op d);                                     \
+    RESET();                                                            \
+    temp_double = Value(xd);                                            \
+    temp_double_2 = temp_double op d;                                   \
+    RESET();                                                            \
+    BOOST_CHECK_EQUAL(temp_double_1, temp_double_2);                    \
+                                                                        \
+    temp_double_1 = Value(xd op xd);                                    \
+    RESET();                                                            \
+    temp_double = Value(xd);                                            \
+    temp_double_2 = temp_double op temp_double;                         \
+    RESET();                                                            \
+    BOOST_CHECK_EQUAL(temp_double_1, temp_double_2)
 
 #define COMPARISON_ITERATION(op)                                 \
     BOOST_CHECK_EQUAL(i op x, i op Value(x));                    \
@@ -65,6 +117,7 @@ BOOST_AUTO_TEST_CASE( arithmetic )
     ARITHMETIC_ITERATION(+);
     ARITHMETIC_ITERATION(-);
     ARITHMETIC_ITERATION(*);
+
     BOOST_CHECK_EQUAL(Value(x / i), Value(x) / i);
     BOOST_CHECK_EQUAL(Value(x / d), Value(x) / d);
     BOOST_CHECK_EQUAL(Value(x / x), Value(x) / Value(x));
@@ -74,10 +127,27 @@ BOOST_AUTO_TEST_CASE( arithmetic )
     BOOST_CHECK_EQUAL(Value(xd / d), Value(xd) / d);
     BOOST_CHECK_EQUAL(Value(xd / xd), Value(xd) / Value(xd));
 
+    BOOST_CHECK_EQUAL(Value(x % i), Value(x) % i);
+    BOOST_CHECK_EQUAL(Value(x % x), Value(x) % Value(x));
+
     ASSIGN_ARITHMETIC_ITERATION(+=);
     ASSIGN_ARITHMETIC_ITERATION(-=);
     ASSIGN_ARITHMETIC_ITERATION(*=);
     ASSIGN_ARITHMETIC_ITERATION(/=);
+
+    temp_int_1 = Value(x %= i);
+    RESET();
+    temp_int = Value(x);
+    temp_int_2 = temp_int %= i;
+    RESET();
+    BOOST_CHECK_EQUAL(temp_int_1, temp_int_2);
+
+    temp_int_1 = Value(x %= x);
+    RESET();
+    temp_int = Value(x);
+    temp_int_2 = temp_int %= temp_int;
+    RESET();
+    BOOST_CHECK_EQUAL(temp_int_1, temp_int_2);
 }
 
 BOOST_AUTO_TEST_CASE( comparison )
@@ -92,19 +162,56 @@ BOOST_AUTO_TEST_CASE( comparison )
 
 BOOST_AUTO_TEST_CASE( unary )
 {
-    BOOST_CHECK_EQUAL(++x, ++(temp_int = Value(x)));
-    BOOST_CHECK_EQUAL(++xd, ++(temp_double = Value(xd)));
-    BOOST_CHECK_EQUAL(x++, (temp_int = Value(x))++);
-    BOOST_CHECK_EQUAL(xd++, (temp_double = Value(xd))++);
-    BOOST_CHECK_EQUAL(x, temp_int);
-    BOOST_CHECK_EQUAL(xd, temp_double);
+    GG::X temp_x_1;
+    GG::X_d temp_xd_1;
 
-    BOOST_CHECK_EQUAL(--x, --(temp_int = Value(x)));
-    BOOST_CHECK_EQUAL(--xd, --(temp_double = Value(xd)));
-    BOOST_CHECK_EQUAL(x--, (temp_int = Value(x))--);
-    BOOST_CHECK_EQUAL(xd--, (temp_double = Value(xd))--);
-    BOOST_CHECK_EQUAL(x, temp_int);
-    BOOST_CHECK_EQUAL(xd, temp_double);
+    temp_x_1 = ++x;
+    RESET();
+    temp_int_2 = ++(temp_int = Value(x));
+    RESET();
+    BOOST_CHECK_EQUAL(temp_x_1, temp_int_2);
+
+    temp_xd_1 = ++xd;
+    RESET();
+    temp_double_2 = ++(temp_double = Value(xd));
+    RESET();
+    BOOST_CHECK_EQUAL(temp_xd_1, temp_double_2);
+
+    temp_x_1 = x++;
+    RESET();
+    temp_int_2 = (temp_int = Value(x))++;
+    RESET();
+    BOOST_CHECK_EQUAL(temp_x_1, temp_int_2);
+
+    temp_xd_1 = xd++;
+    RESET();
+    temp_double_2 = (temp_double = Value(xd))++;
+    RESET();
+    BOOST_CHECK_EQUAL(temp_xd_1, temp_double_2);
+
+    temp_x_1 = --x;
+    RESET();
+    temp_int_2 = --(temp_int = Value(x));
+    RESET();
+    BOOST_CHECK_EQUAL(temp_x_1, temp_int_2);
+
+    temp_xd_1 = --xd;
+    RESET();
+    temp_double_2 = --(temp_double = Value(xd));
+    RESET();
+    BOOST_CHECK_EQUAL(temp_xd_1, temp_double_2);
+
+    temp_x_1 = x--;
+    RESET();
+    temp_int_2 = (temp_int = Value(x))--;
+    RESET();
+    BOOST_CHECK_EQUAL(temp_x_1, temp_int_2);
+
+    temp_xd_1 = xd--;
+    RESET();
+    temp_double_2 = (temp_double = Value(xd))--;
+    RESET();
+    BOOST_CHECK_EQUAL(temp_xd_1, temp_double_2);
 
     BOOST_CHECK_EQUAL(!x, !Value(x));
     BOOST_CHECK_EQUAL(!xd, !Value(xd));

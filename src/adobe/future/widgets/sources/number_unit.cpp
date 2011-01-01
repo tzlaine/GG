c091db811ec84c96759490cb389163a95ebb62ba
T. Zachary Laine <whatwasthataddress@gmail.com>
2011-01-01 03:41:08 +0000

    Grooming.

--- src/adobe/future/widgets/sources/number_unit.cpp
+++ src/adobe/future/widgets/sources/number_unit.cpp
@@ -26,13 +26,13 @@ unit_t to_unit(const dictionary_t& dict, const unit_t& default_unit)
     unit_t  single_unit(default_unit);
     array_t scale;
 
-    get_value(dict,             key_name,                               single_unit.name_m);
-        get_value(dict,         key_short_name,                 single_unit.short_name_m);
-    get_value(dict,             key_bind,                               single_unit.base_unit_m);
-    get_value(dict,             key_format,                             single_unit.format_m);
-        get_value(dict,         key_min_max_filter,             single_unit.base_unit_filter_m);
+    get_value(dict,             key_name,                       single_unit.name_m);
+    get_value(dict,             key_short_name,                 single_unit.short_name_m);
+    get_value(dict,             key_bind,                       single_unit.base_unit_m);
+    get_value(dict,             key_format,                     single_unit.format_m);
+    get_value(dict,             key_min_max_filter,             single_unit.base_unit_filter_m);
     get_value(dict,             key_decimal_places,             single_unit.decimal_places_m);
-        get_value(dict,         key_trailing_zeroes,    single_unit.trailing_zeroes_m);
+    get_value(dict,             key_trailing_zeroes,            single_unit.trailing_zeroes_m);
     get_value(dict,             key_increment,                  single_unit.increment_m);
     get_value(dict,             key_min_value,                  single_unit.min_value_m);
     get_value(dict,             key_max_value,                  single_unit.max_value_m);
